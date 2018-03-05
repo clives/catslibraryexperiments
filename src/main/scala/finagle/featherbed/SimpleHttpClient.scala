59786b08665e073637a44a64190e843ac548b8b9
sosthene <clivearg@gmail.com>
2018-03-05 16:39:36 -0300

    simple http get client

---
+++ src/main/scala/finagle/featherbed/SimpleHttpClient.scala
@@ -0,0 +1,43 @@
+package finagle.featherbed
+
+import com.twitter.finagle.http.Response
+import com.twitter.util.{Await, Future}
+import featherbed.request.{ErrorResponse, InvalidResponse}
+
+
+
+trait SimpleHttpClient {
+  import java.net.URL
+
+  val client = new featherbed.Client(new URL("http://httpbin.org/"))
+  val VALID_SERVICE ="uuid";
+  val INVALID_SERVICE ="uuuid"
+
+
+  def testGetClients() = {
+    val simpleGetRequest =  client.get(VALID_SERVICE).send[Response]()
+    val simpleErrorGetRequest = client.get(INVALID_SERVICE).send[Response]()
+
+    Await.result {
+
+      val f1:Future[Unit]= simpleGetRequest map {
+        response =>
+          println("response" + response.contentString)
+      }
+
+      val f2= simpleErrorGetRequest.map(_.contentString).map(Right.apply).handle {
+        case err @ ErrorResponse(rep, reason) => Left(err)
+      }.map{
+        case Left( error ) => println(s"Error: $error")
+        case Right( content ) => println(s"Sucess: $content")
+      }
+
+      // !! TwitterFuture
+      Future.collect(Seq(f1,f2))
+    }
+  }
+}
+
+object SimpleHttpClient extends SimpleHttpClient with App {
+  testGetClients()
+}
